ff15040cee1026bd1224367702454e1ebae9f73f
Hector Roussille <hector.roussille@gmail.com>
2016-10-21 14:02:28 +0200

    added comments in module_parameters.c

--- 02-Module-Parameters/module_parameters.c
+++ 02-Module-Parameters/module_parameters.c
@@ -3,25 +3,55 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 
+/* Declare module license , author and description */
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Hector Roussille <hector.roussille@gmail.com");
 MODULE_DESCRIPTION("Basic module with parameters");
 
+/* Declare the variabe that we will use as an interger parameter */
+
 static int param_int = 10;
+
+/* 
+ * Declare the previous variable as an interger parameter then
+ * place a description for this parameter
+ */
 
 module_param(param_int, int, 0600);
 MODULE_PARM_DESC(param_int, "Integer parameter");
 
+/* Declare the variable that we will use as a string parameter */
+
 static char *param_string = "test string";
+
+/*
+ * Declare the previous variable as a string parameter then
+ * place a description for this parameter
+ */
 
 module_param(param_string, charp, 0600);
 MODULE_PARM_DESC(param_string, "String parameter");
 
+/*
+ * Declare the variable that we will use as an interger array parameter
+ * Also declare the variable that holds the number of elements presents in
+ * the array
+ */
+
 static int param_int_array[2] = { 0, 0 };
 static int elements_in_array;
 
+/* Declare the previous variables as an integer array parameter and set a
+ * pointer to the variable holding the number of elements currently presents
+ * int it
+ * Then place a description for this parameter
+ */
+
 module_param_array(param_int_array, int, &elements_in_array, 0600);
 MODULE_PARM_DESC(param_int_array, "Integer array parameter");
+
+/* print_params : prints the parameters values */
 
 void print_params(void)
 {
